aac23ef6bafd56efa796e012f497655e6e823049
Gabriella Guerra <gabriellafguerra21@gmail.com>
2024-10-12 10:18:37 -0300

    rotas para gerenciamento das contas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BillController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\UserRole;
 use Illuminate\Support\Facades\Route;
@@ -8,16 +9,12 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', UserRole::class])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/bills', BillController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
