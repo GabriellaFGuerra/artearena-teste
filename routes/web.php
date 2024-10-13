fed0b753a15fcf6af7b803f5b1d4455b880d203b
Gabriella Guerra <gabriellafguerra21@gmail.com>
2024-10-12 23:36:56 -0300

    adicionada rota para live search de usuários

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\BillController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/bills');
 });
 
 Route::middleware('auth')->group(function () {
@@ -14,6 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('/bills', BillController::class);
+    Route::get('/search-users', [UserController::class, 'searchUsers'])->name('users.search');
 });
 
 require __DIR__ . '/auth.php';
